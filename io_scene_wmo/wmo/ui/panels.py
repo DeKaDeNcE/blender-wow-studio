92623c03a6fe9e45f87906630778744db5bdd87e
Skarn <skarnproject@gmail.com>
2018-01-13 01:00:45 +0300

    move game data connector to the topbar

--- io_scene_wmo/wmo/ui/panels.py
+++ io_scene_wmo/wmo/ui/panels.py
@@ -1059,11 +1059,6 @@ class WMOToolsPanelObjectMode(bpy.types.Panel):
         else:
             box1_col.operator("scene.wow_add_scale_reference", text = 'Scale', icon = 'OUTLINER_OB_ARMATURE')
 
-        col.label(text="Game data:")
-        icon = 'COLOR_GREEN' if game_data_loaded else 'COLOR_RED'
-        text = "Unload game data" if game_data_loaded else "Load game data"
-        col.operator("scene.load_wow_filesystem", text=text, icon = icon)
-
         if bpy.context.selected_objects:
             col.separator()
             col.label(text="Selected:")
@@ -1156,6 +1151,15 @@ class WoWToolsPanelLiquidFlags(bpy.types.Panel):
                 and context.object.WowLiquid.Enabled
                 )
 
+def render_gamedata_toggle(self, context):
+    game_data_loaded = hasattr(bpy, "wow_game_data") and bpy.wow_game_data.files
+
+    layout = self.layout
+    row = layout.row(align=True)
+    icon = 'COLOR_GREEN' if game_data_loaded else 'COLOR_RED'
+    text = "Disconnect WoW" if game_data_loaded else "Connect WoW"
+    row.operator("scene.load_wow_filesystem", text=text, icon=icon)
+
 def register():
     RegisterWowRootProperties()
     RegisterWoWDoodadProperties()
@@ -1168,6 +1172,7 @@ def register():
     RegisterWoWVisibilityProperties()
     RegisterWowFogProperties()
     bpy.types.INFO_MT_add.prepend(wow_components_add_menu_item)
+    bpy.types.INFO_HT_header.append(render_gamedata_toggle)
 
 def unregister():
     UnregisterWowRootProperties()
@@ -1181,6 +1186,7 @@ def unregister():
     UnregisterWoWVisibilityProperties()
     UnregisterWowFogProperties()
     bpy.types.INFO_MT_add.remove(wow_components_add_menu_item)
+    bpy.types.INFO_HT_header.remove(render_gamedata_toggle)
 
 
 
