7e86e2e6457e28c01c0d491fb44b6711d6d51482
Skarn <skarnproject@gmail.com>
2018-02-10 00:22:05 +0300

    fix error

--- io_scene_wmo/m2/import_m2.py
+++ io_scene_wmo/m2/import_m2.py
@@ -284,7 +284,7 @@ class BlenderM2Scene:
 
             obj.location = self.rig.data.bones[bone.name].matrix_local.inverted() * Vector(attachment.position)
 
-            obj.name = M2AttachmentTypes.get_attachment_name(attachment, i)
+            obj.name = M2AttachmentTypes.get_attachment_name(attachment.id, i)
 
     def load_collision(self):
 
