06b83d98ca64c3fd579afe00607f871d9b2419c0
Skarn <skarnproject@gmail.com>
2018-05-13 12:32:27 +0300

    make UI lists point to correct data-blocks

--- io_scene_wmo/m2/animation_editor/__init__.py
+++ io_scene_wmo/m2/animation_editor/__init__.py
@@ -45,21 +45,33 @@ class AnimationEditorDialog(bpy.types.Operator):
 
             row = col.row()
             sub_col1 = row.column()
-            sub_col1.template_list("AnimationEditor_SequenceObjectList", "", context.scene, "WowM2Animations", context.scene,
-                                   "WowM2CurAnimIndex")
+            sub_col1.template_list("AnimationEditor_SequenceObjectList", "", cur_anim_track, "AnimPairs",
+                                   cur_anim_track, "ActiveObjectIndex")
             sub_col2 = row.column(align=True)
             sub_col2.operator("scene.wow_m2_animation_editor_seq_add", text='', icon='ZOOMIN')
             sub_col2.operator("scene.wow_m2_animation_editor_seq_remove", text='', icon='ZOOMOUT')
 
             col = split.column()
             col.label('NLA Tracks:', icon='NLA')
-            row = col.row()
-            sub_col1 = row.column()
-            sub_col1.template_list("AnimationList", "", context.scene, "WowM2Animations", context.scene,
-                                   "WowM2CurAnimIndex")
-            sub_col2 = row.column(align=True)
-            sub_col2.operator("scene.wow_m2_animation_editor_seq_add", text='', icon='ZOOMIN')
-            sub_col2.operator("scene.wow_m2_animation_editor_seq_remove", text='', icon='ZOOMOUT')
+
+            try:
+                cur_anim_pair = cur_anim_track[cur_anim_track.ActiveObjectIndex]
+            except IndexError:
+                col.label('No object selected.', icon='ERROR')
+
+            if cur_anim_pair.Object is not None:
+
+                row = col.row()
+                sub_col1 = row.column()
+                sub_col1.template_list("AnimationEditor_ObjectNLATrackList", "", cur_anim_pair, "NLATracks",
+                                       cur_anim_pair, "ActiveTrack")
+                sub_col2 = row.column(align=True)
+                sub_col2.operator("scene.wow_m2_animation_editor_seq_add", text='', icon='ZOOMIN')
+                sub_col2.operator("scene.wow_m2_animation_editor_seq_remove", text='', icon='ZOOMOUT')
+
+            else:
+                col.label('Active object slot is empty.', icon='ERROR')
+
 
         else:
 
@@ -77,59 +89,6 @@ class AnimationEditorDialog(bpy.types.Operator):
 # Animation List
 
 class AnimationEditor_AnimationList(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
-        ob = data
-        if self.layout_type in {'DEFAULT', 'COMPACT'}:
-            layout.prop(item, "name", text="", emboss=False, icon_value=icon)
-        elif self.layout_type in {'GRID'}:
-            pass
-
-
-class AnimationEditor_ObjectAdd(bpy.types.Operator):
-    bl_idname = 'scene.wow_m2_animation_editor_object_add'
-    bl_label = 'Add object'
-    bl_description = 'Add new object to selected animation sequence'
-    bl_options = {'REGISTER', 'INTERNAL'}
-
-    def execute(self, context):
-        scene = context.scene
-
-        try:
-            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
-            sequence.AnimPairs.add()
-
-        except IndexError:
-            self.report({'ERROR'}, "No animation sequence selected")
-            return {'CANCELLED'}
-
-        return {'FINISHED'}
-
-
-class AnimationEditor_ObjectRemove(bpy.types.Operator):
-    bl_idname = 'scene.wow_m2_animation_editor_object_remove'
-    bl_label = 'Remove object'
-    bl_description = 'Remove object from selected animation sequence'
-    bl_options = {'REGISTER', 'INTERNAL'}
-
-    def execute(self, context):
-
-        scene = context.scene
-
-        try:
-            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
-            sequence.AnimPairs.remove()
-
-        except IndexError:
-            self.report({'ERROR'}, "No animation sequence selected")
-            return {'CANCELLED'}
-
-        return {'FINISHED'}
-
-
-# Object list
-
-
-class AnimationEditor_SequenceObjectList(bpy.types.UIList):
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
         ob = data
         if self.layout_type in {'DEFAULT', 'COMPACT'}:
@@ -163,6 +122,58 @@ class AnimationEditor_SequenceRemove(bpy.types.Operator):
         return {'FINISHED'}
 
 
+# Object list
+
+
+class AnimationEditor_SequenceObjectList(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
+        ob = data
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
+            layout.prop(item, "name", text="", emboss=False, icon_value=icon)
+        elif self.layout_type in {'GRID'}:
+            pass
+
+class AnimationEditor_ObjectAdd(bpy.types.Operator):
+    bl_idname = 'scene.wow_m2_animation_editor_object_add'
+    bl_label = 'Add object'
+    bl_description = 'Add new object to selected animation sequence'
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context):
+
+        scene = context.scene
+
+        try:
+            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
+            sequence.AnimPairs.add()
+
+        except IndexError:
+            self.report({'ERROR'}, "No animation sequence selected")
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+
+class AnimationEditor_ObjectRemove(bpy.types.Operator):
+    bl_idname = 'scene.wow_m2_animation_editor_object_remove'
+    bl_label = 'Remove object'
+    bl_description = 'Remove object from selected animation sequence'
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context):
+
+        scene = context.scene
+
+        try:
+            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
+            sequence.AnimPairs.remove()
+
+        except IndexError:
+            self.report({'ERROR'}, "No animation sequence selected")
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+
+
 # NLA Track list
 
 
@@ -176,13 +187,34 @@ class AnimationEditor_ObjectNLATrackList(bpy.types.UIList):
 
 
 class AnimationEditor_NLATrackSlotAdd(bpy.types.Operator):
-    bl_idname = 'scene.wow_m2_animation_editor_seq_add'
-    bl_label = 'Add WoW animation'
-    bl_description = 'Add Wow animation sequence'
+    bl_idname = 'scene.wow_m2_animation_editor_nla_track_add'
+    bl_label = 'Add NLA track slot'
+    bl_description = 'Add NLA track slot to selected object'
     bl_options = {'REGISTER', 'INTERNAL'}
 
     def execute(self, context):
-        sequence = context.scene.WowM2Animations.add()
+        scene = context.scene
+
+        try:
+            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
+
+            try:
+                anim_pair = sequence.AnimPairs[sequence.ActiveObjectIndex]
+
+                if anim_pair.Object is None:
+                    self.report({'ERROR'}, "Active object slot is empty")
+                    return {'CANCELLED'}
+
+                nla_track = anim_pair.NLATracks.add()
+
+
+            except IndexError:
+                self.report({'ERROR'}, "No object selected in this sequence")
+                return {'CANCELLED'}
+
+        except IndexError:
+            self.report({'ERROR'}, "No animation sequence selected")
+            return {'CANCELLED'}
 
         return {'FINISHED'}
 
@@ -195,7 +227,28 @@ class AnimationEditor_NLATrackSlotRemove(bpy.types.Operator):
 
     def execute(self, context):
 
-        context.scene.WowM2Animations.remove(context.scene.WowM2CurAnimIndex)
+        scene = context.scene
+
+        try:
+            sequence = scene.WowM2Animations[scene.WowM2CurAnimIndex]
+
+            try:
+                anim_pair = sequence.AnimPairs[sequence.ActiveObjectIndex]
+
+                if anim_pair.Object is None:
+                    self.report({'ERROR'}, "Active object slot is empty")
+                    return {'CANCELLED'}
+
+                nla_track = anim_pair.NLATracks.add()
+
+
+            except IndexError:
+                self.report({'ERROR'}, "No object selected in this sequence")
+                return {'CANCELLED'}
+
+        except IndexError:
+            self.report({'ERROR'}, "No animation sequence selected")
+            return {'CANCELLED'}
 
         return {'FINISHED'}
 
@@ -240,12 +293,14 @@ class WowM2AnimationEditorAnimationPairsPropertyGroup(bpy.types.PropertyGroup):
         description="NLA Tracks to use in this animation sequence"
     )
 
-    ActiveObjectIndex = bpy.props.IntProperty()
+    ActiveTrack = bpy.props.IntProperty()
 
 
 class WowM2AnimationEditorPropertyGroup(bpy.types.PropertyGroup):
 
     AnimPairs = bpy.props.CollectionProperty(type=WowM2AnimationEditorAnimationPairsPropertyGroup)
+
+    ActiveObjectIndex = bpy.props.IntProperty()
 
 
 def register_wow_m2_animation_editor_properties():
