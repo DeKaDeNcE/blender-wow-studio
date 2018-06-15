6d8733ee383d0c8632e4bcb764cd3154d72ab6bd
Skarn <skarnproject@gmail.com>
2018-06-15 18:36:05 +0300

    add operators to compose and decompose path curves

--- io_scene_wmo/m2/ui/panels/camera.py
+++ io_scene_wmo/m2/ui/panels/camera.py
@@ -118,6 +118,12 @@ class WowM2CameraPanel(bpy.types.Panel):
         sub_col3.operator("object.wow_m2_camera_curve_move", text='', icon='TRIA_UP').direction = 'UP'
         sub_col3.operator("object.wow_m2_camera_curve_move", text='', icon='TRIA_DOWN').direction = 'DOWN'
 
+        split = col.row().split(percentage=0.94)
+        row_split = split.row(align=True)
+        row_split.operator("object.wow_m2_camera_curve_compose", text='Compose curve', icon='RNDCURVE')
+        row_split.operator("object.wow_m2_camera_curve_decompose", text='Decompose curve', icon='CURVE_PATH')
+        split.row()
+
         # draw error box
         errors = sorted(validate_camera_path(context.object.wow_m2_camera), key=lambda x: x[1])
         if errors:
@@ -209,8 +215,7 @@ class WowM2Camera_CurveList(bpy.types.UIList):
 
 class WowM2Camera_CurveAdd(bpy.types.Operator):
     bl_idname = 'object.wow_m2_camera_curve_add'
-    bl_label = 'Add WoW animation'
-    bl_description = 'Add WoW animation sequence'
+    bl_label = 'Add curve segment'
     bl_options = {'REGISTER', 'INTERNAL'}
 
     def execute(self, context):
@@ -222,8 +227,7 @@ class WowM2Camera_CurveAdd(bpy.types.Operator):
 
 class WowM2Camera_CurveRemove(bpy.types.Operator):
     bl_idname = 'object.wow_m2_camera_curve_remove'
-    bl_label = 'Remove WoW animation'
-    bl_description = 'Remove WoW animation sequence'
+    bl_label = 'Remove curve segment'
     bl_options = {'REGISTER', 'INTERNAL'}
 
     def execute(self, context):
@@ -234,8 +238,7 @@ class WowM2Camera_CurveRemove(bpy.types.Operator):
 
 class WowM2Camera_CurveMove(bpy.types.Operator):
     bl_idname = 'object.wow_m2_camera_curve_move'
-    bl_label = 'Move WoW animation'
-    bl_description = 'Move WoW animation sequence'
+    bl_label = 'Move curve segment'
     bl_options = {'REGISTER', 'INTERNAL'}
 
     direction = bpy.props.StringProperty()
@@ -254,6 +257,152 @@ class WowM2Camera_CurveMove(bpy.types.Operator):
 
         else:
             raise NotImplementedError("Only UP and DOWN movement in the UI list in supported.")
+
+        return {'FINISHED'}
+
+
+class WowM2Camera_CurveCompose(bpy.types.Operator):
+    bl_idname = 'object.wow_m2_camera_curve_compose'
+    bl_label = 'Compose curve'
+    bl_description = 'Convert current curves to a single curve'
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    def execute(self, context):
+
+        if validate_camera_path(context.object.wow_m2_camera):  # checking for errors
+            self.report({'ERROR'}, 'M2 curve path contains errors.')
+            return {'CANCELLED'}
+
+        camera_props = context.object.wow_m2_camera
+
+        curve = bpy.data.curves.new(name='BakedCurve', type='CURVE')
+        curve_obj = bpy.data.objects.new(name='BakedCurve', object_data=curve)
+        bpy.context.scene.objects.link(curve_obj)
+        curve.dimensions = '3D'
+        curve.resolution_u = 64
+
+        spline = curve.splines.new('BEZIER')
+        spline.resolution_u = 64
+        spline.bezier_points.add(count=len(camera_props.animation_curves))
+
+        curve_first_point = spline.bezier_points[0]
+        segment_first_point = camera_props.animation_curves[0].object.data.splines[0].bezier_points[0]
+
+        curve_first_point.co = segment_first_point.co
+        curve_first_point.handle_right = segment_first_point.handle_right
+        curve_first_point.handle_left = segment_first_point.handle_left
+
+        for i, segment in enumerate(camera_props.animation_curves):
+            segment_point = segment.object.data.splines[0].bezier_points[1]
+            curve_point = spline.bezier_points[i + 1]
+            curve_point.co = segment_point.co
+            curve_point.handle_right = segment_point.handle_right
+            curve_point.handle_left = segment_point.handle_left
+
+        return {'FINISHED'}
+
+
+class WowM2Camera_CurveDecompose(bpy.types.Operator):
+    bl_idname = 'object.wow_m2_camera_curve_decompose'
+    bl_label = 'Decompose curve'
+    bl_description = 'Convert a single curves to timed curve segments'
+    bl_options = {'REGISTER', 'INTERNAL'}
+
+    curve_obj = bpy.props.StringProperty(
+        name='Curve',
+        description='Curve object to be decomposed to timed curve segments'
+    )
+
+    preserve_timing = bpy.props.BoolProperty(
+        name='Preserve Timing',
+        description='Align decomposed curve segments to already existing timed slots.',
+        default=True
+    )
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop_search(self, 'curve_obj', context.scene, 'objects', icon='OBJECT_DATA')
+        layout.prop(self, 'preserve_timing')
+
+    def execute(self, context):
+
+        try:
+            curve_obj = bpy.data.objects[self.curve_obj]
+        except KeyError:
+            self.report({'ERROR'}, 'No curve object is selected.')
+            return {'FINISHED'}
+
+        if curve_obj.type != 'CURVE':
+            self.report({'ERROR'}, 'Select a bezier curve object instead.')
+            return {'FINISHED'}
+
+        if not curve_obj.data.splines or len(curve_obj.data.splines[0].bezier_points) < 2:
+            self.report({'ERROR'}, 'Badly formed curve.')
+            return {'FINISHED'}
+
+        if not curve_obj.data.splines[0].type == 'BEZIER':
+            self.report({'ERROR'}, 'Select a bezier curve object instead.')
+            return {'FINISHED'}
+
+        camera_props = context.object.wow_m2_camera
+
+        # remove old referenced segment objects
+        for segment in camera_props.animation_curves:
+
+            old_segment = segment.object
+            segment.object = None
+            bpy.data.objects.remove(old_segment, do_unlink=True)
+
+        # remove slots based on user's choice
+        if not self.preserve_timing:
+            camera_props.animation_curves.clear()
+
+        # create segment curves
+        new_segments = []
+        points = curve_obj.data.splines[0].bezier_points
+        for i in range(1, len(points)):
+            first_point = points[i - 1]
+            last_point = points[i - 2]
+
+            name = "{}_BakedSegment".format(curve_obj.name)
+            curve = bpy.data.curves.new(name=name, type='CURVE')
+            new_obj = bpy.data.objects.new(name=name, object_data=curve)
+            bpy.context.scene.objects.link(new_obj)
+
+            curve.dimensions = '3D'
+            curve.resolution_u = 64
+
+            spline = curve.splines.new('BEZIER')
+            spline.resolution_u = 64
+            spline.bezier_points.add(count=1)
+
+            seg_first_point = spline.bezier_points[0]
+            seg_last_point = spline.bezier_points[1]
+
+            seg_first_point.co = first_point.co
+            seg_first_point.handle_right = first_point.handle_left
+            seg_first_point.handle_left = first_point.handle_right
+
+            seg_last_point.co = last_point.co
+            seg_last_point.handle_right = last_point.handle_left
+            seg_last_point.handle_left = last_point.handle_right
+
+            new_segments.append(new_obj)
+
+        # fill in overlapping slots
+        segments_overlap = 0
+        for i, segment in enumerate(camera_props.animation_curves):
+            segment.object = new_segments[i]
+            segments_overlap += 1
+
+        # create new slots for extra segments
+        for i in range(segments_overlap, len(new_segments) - segments_overlap):
+            segment = camera_props.animation_curves.add()
+            segment.object = new_segments[i]
 
         return {'FINISHED'}
 
