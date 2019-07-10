f5001579c70e51010c8b1b06dcd84664915d9925
Skarn <skarnproject@gmail.com>
2019-07-10 17:30:36 +0300

    Lock depsgraph on import to prevent code running concurrently

--- io_scene_wmo/wmo/import_wmo.py
+++ io_scene_wmo/wmo/import_wmo.py
@@ -5,6 +5,7 @@ from .wmo_scene import BlenderWMOScene
 from ..pywowlib import CLIENT_VERSION
 from ..pywowlib.wmo_file import WMOFile
 from ..ui import get_addon_prefs
+from .ui.handlers import DepsgraphLock
 
 
 def import_wmo_to_blender_scene(filepath, import_doodads, import_lights, import_fogs, group_objects):
@@ -17,21 +18,22 @@ def import_wmo_to_blender_scene(filepath, import_doodads, import_lights, import_
     addon_prefs = get_addon_prefs()
     game_data = load_game_data()
 
-    wmo = WMOFile(CLIENT_VERSION, filepath=filepath)
-    wmo.read()
-    wmo_scene = BlenderWMOScene(wmo=wmo, prefs=addon_prefs)
+    with DepsgraphLock():
+        wmo = WMOFile(CLIENT_VERSION, filepath=filepath)
+        wmo.read()
+        wmo_scene = BlenderWMOScene(wmo=wmo, prefs=addon_prefs)
 
-    # extract textures to cache folder
-    game_data.extract_textures_as_png(addon_prefs.cache_dir_path, wmo.motx.get_all_strings())
+        # extract textures to cache folder
+        game_data.extract_textures_as_png(addon_prefs.cache_dir_path, wmo.motx.get_all_strings())
 
-    # load all WMO components
-    wmo_scene.load_materials()
-    wmo_scene.load_lights()
-    wmo_scene.load_properties()
-    wmo_scene.load_fogs()
-    wmo_scene.load_groups()
-    wmo_scene.load_portals()
-    wmo_scene.load_doodads(assets_dir=addon_prefs.cache_dir_path)
+        # load all WMO components
+        wmo_scene.load_materials()
+        wmo_scene.load_lights()
+        wmo_scene.load_properties()
+        wmo_scene.load_fogs()
+        wmo_scene.load_groups()
+        wmo_scene.load_portals()
+        wmo_scene.load_doodads(assets_dir=addon_prefs.cache_dir_path)
 
     print("\nDone importing WMO. \nTotal import time: ",
           time.strftime("%M minutes %S seconds.\a", time.gmtime(time.time() - start_time)))
--- io_scene_wmo/wmo/ui/handlers.py
+++ io_scene_wmo/wmo/ui/handlers.py
@@ -205,6 +205,7 @@ def on_depsgraph_update(_):
 
                         # enforce object mode or sculpt mode
                         elif obj.mode not in ('OBJECT', 'SCULPT'):
+                            bpy.context.view_layer.objects.active = obj
                             bpy.ops.object.mode_set(mode='OBJECT')
 
 
@@ -224,9 +225,11 @@ def on_depsgraph_update(_):
                 elif update.id.wow_wmo_fog.enabled:
                     obj = bpy.data.objects[update.id.name, update.id.library]
 
-                    # enforce object mode
-                    if obj.mode == 'EDIT':
-                        bpy.ops.object.mode_set(mode='OBJECT')
+                    with DepsgraphLock():
+                        # enforce object mode
+                        if obj.mode != 'OBJECT':
+                            bpy.context.view_layer.objects.active = obj
+                            bpy.ops.object.mode_set(mode='OBJECT')
 
 
             elif isinstance(update.id, bpy.types.Scene):
