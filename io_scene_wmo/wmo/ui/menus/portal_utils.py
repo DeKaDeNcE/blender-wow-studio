49446ace0c332fea6039af0802ed83b9133fdedd
Skarn <skarnproject@gmail.com>
2020-04-23 21:11:53 +0300

    fix issue #42

--- io_scene_wmo/wmo/ui/menus/portal_utils.py
+++ io_scene_wmo/wmo/ui/menus/portal_utils.py
@@ -10,7 +10,5 @@ class VIEW3D_MT_wmo_portal_actions(Menu):
         layout = self.layout
         pie = layout.menu_pie()
 
-        pie.operator("scene.wow_set_portal_dir_alg", text='Set portal direction',
-                     icon_value=ui_icons['WOW_STUDIO_APPLY_DIRECTION'])
-        pie.operator("scene.wow_bake_portal_relations", text='Bake portal relations',
-                     icon_value=ui_icons['WOW_STUDIO_APPLY_RELATIONS'])
+        pie.operator("scene.wow_set_portal_dir_alg", text='Set portal direction', icon='INDIRECT_ONLY_ON')
+        pie.operator("scene.wow_bake_portal_relations", text='Bake portal relations', icon='LIBRARY_DATA_DIRECT')
